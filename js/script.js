0f81787608e5e7641d29ae2c8d3ae359eeee1d8a
Dandy Farid <dandyf@koito97.org>
2020-10-29 19:32:30 +0700

    Add overlay in sidebar
    Signed-off-by: Dandy Farid <dandyf@koito97.org>

--- js/script.js
+++ js/script.js
@@ -107,8 +107,10 @@ function atasFunction() {
 
 function w3_open() {
     document.getElementById("koito97Sidebar").style.display = "block";
+    document.getElementById("koito97Overlay").style.display = "block";
 }
 
 function w3_close() {
     document.getElementById("koito97Sidebar").style.display = "none";
+    document.getElementById("koito97Overlay").style.display = "none";
 }
