07d07f2f7eff3f93713d6204be43680bbc1206ee
Dandy Farid <dan@koito97.co>
2020-12-26 10:11:19 +0700

    [temp] Remove the sidebar for now
    Signed-off-by: Dandy Farid <dan@koito97.co>

--- js/script.js
+++ js/script.js
@@ -14,13 +14,3 @@ function atasFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-
-function w3_open() {
-    document.getElementById("koito97Sidebar").style.display = "block";
-    document.getElementById("koito97Overlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("koito97Sidebar").style.display = "none";
-    document.getElementById("koito97Overlay").style.display = "none";
-}
